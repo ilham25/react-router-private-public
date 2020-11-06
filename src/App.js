05c6e17f5d2e4632eeaf82af0d2023a5f282bcea
ilham25 <ilhamadiputra4@gmail.com>
2020-11-06 15:31:04 +0700

    add private route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 // import "./App.css";
 
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <PrivateRoute component={Dashboard} path="/dashboard" exact />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
